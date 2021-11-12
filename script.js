2f97ed10dbc3cfb18f77ed0077e2a4950dbc48b0
haydo <haydent2013is10@gmail.com>
2021-11-12 07:49:07 -0700

    Push 5

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-async function fetchData(character) {
+// async function fetchData(character) {
+//   try {
+//     let url = await fetch(`https://rickandmortyapi.com/api/character`);
+//     let user = await response.json();
+//     newfunction();
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// axios
+//   .get("https://rickandmortyapi.com/api/character")
+//   .then((res) => {
+//   console.log(res.data.results);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+// })
+
+
+// function fetchData(name) {
+//   const url = `https://rickandmortyapi.com/api/character`;
+//   axios.get(url).then((res) => {
+//     console.log(res.data);
+//   }).catch((error) => {
+//     console.log(error);
+//   });
+// }
+
+// fetchData("Rick");
+
+async function fetchData(name) {
+  const url = `https://rickandmortyapi.com/api/character`;
   try {
-    let url = await fetch(`https://rickandmortyapi.com/api/character`);
-    let user = await response.json();
-    newfunction();
+    const res = await axios.get(url);
+    const charData = res.data;
+    showCharData(charData);
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 }
+
+function showCharData(data) {
+  console.log(data);
+  const charName = document.createElement("h2");
+  charName.innerText = data.name;
+}
+
+
+fetchData("Rick");
